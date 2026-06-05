d0cf84dd1a80be9b6b54786880cb97e14356b3a0
Ahmedjalalz <Ahmedjalalzen@gmail.com>
2026-06-05 20:05:12 +0500

    chore: add tailwindcss plugin to app.config.ts to fix styling on vercel

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "@tanstack/react-start/config";
+import tailwindcss from "@tailwindcss/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
     preset: "vercel",
   },
   vite: {
+    plugins: [
+      tailwindcss(),
+      tsConfigPaths()
+    ],
     ssr: {
       noExternal: [
         "@tanstack/react-router",
